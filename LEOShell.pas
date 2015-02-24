90da7f05a014290d8f17891ede2f1c6d86e14d7f
Valentín <yafueva@gmail.com>
2015-02-24 07:17:27 -0300

    Versión estable.

---
+++ LEOShell.pas
@@ -0,0 +1,37 @@
+{
+    LEOShell - Intérprete de comandos experimental
+
+    Copyright (C) 2015 - Valentín Costa - Leandro Lonardi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
+
+program LEOShell;
+
+uses Comandos, Controles;
+    
+begin
+    inicializar();
+
+    leer( entrada );
+
+    while entrada <> 'exit' do
+    begin
+        comando := convertir( entrada );
+
+        ejecutar( comando );
+
+        leer( entrada );
+    end;
+end.
